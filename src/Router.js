cd0b1fc3759a7f2dab303ff801c7472163fd4ced
FShinoda <fernanda.shinoda@hotmail.com>
2021-04-22 18:24:40 -0300

    folder structure done

---
+++ src/Router.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Switch, Route } from 'react-router-dom';
+
+/// Pages
+// Pagina App - exemplo
+import App from './pages/exemplo/App';
+
+const Router = () =>{
+    return(
+        <Switch>
+            <Route exact path="/exemplo" component={App}/>
+        </Switch>
+    );
+};
+
+export default Router;
