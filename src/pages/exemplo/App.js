2cde3a860f4d660f60916b479e77128698053199
João Kitajima <joao.kitajima@outlook.com>
2021-04-23 15:46:37 -0300

    smaccs

---
+++ src/pages/exemplo/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+const App = () =>{
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Java</h1>
+        <code>
+          x = 3;
+          y = 4;
+
+          x == 4 -- False
+          x === 4 -- False
+        </code>
+      </header>
+    </div>
+  );
+}
+
+export default App;
