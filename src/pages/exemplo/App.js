36a913abfcf75d917bbf31a5857dd037de60a7e4
João Kitajima <joao.kitajima@outlook.com>
2021-04-23 15:46:37 -0300

    smaccs

--- src/pages/exemplo/App.js
+++ src/pages/exemplo/App.js
@@ -5,17 +5,14 @@ const App = () =>{
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Java</h1>
+        <code>
+          x = 3;
+          y = 4;
+
+          x == 4 -- False
+          x === 4 -- False
+        </code>
       </header>
     </div>
   );
