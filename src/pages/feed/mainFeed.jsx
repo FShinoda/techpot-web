62cbcb512e007f3912e60e576676f16707213c00
FShinoda <fernanda.shinoda@hotmail.com>
2021-04-30 14:27:58 -0300

    main page structured

---
+++ src/pages/feed/mainFeed.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './style.css';
+
+const MainFeed = () =>{
+
+    return(
+        <div className="MainFeed">
+            <div className="container-left">
+                <h1>esquerda</h1>
+            </div>
+            <div className="container-center">
+                <h1>feed</h1>
+            </div>
+            <div className="container-right">
+                <h1>direita</h1>
+            </div>
+        </div>
+    )
+}
+
+export default MainFeed;
