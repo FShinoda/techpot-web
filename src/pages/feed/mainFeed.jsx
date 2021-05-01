f5232fd7a859eb3b54c3279b80ac6de8a7d48560
FShinoda <fernanda.shinoda@hotmail.com>
2021-04-30 22:19:46 -0300

    added to main feed fst component

--- src/pages/feed/mainFeed.jsx
+++ src/pages/feed/mainFeed.jsx
@@ -2,18 +2,48 @@ import React from 'react';
 
 import './style.css';
 
+// Icons
+import {PlusOutlined} from '@ant-design/icons';
+
+// Componentes
+import ContainerInfo from './components/containerInfo/containerInfo';
+import ContainerItem from './components/containerItem/containerItem';
+
 const MainFeed = () =>{
 
+    const iconPlus = () =>{
+        <h1>fodasse</h1>
+    }
     return(
         <div className="MainFeed">
             <div className="container-left">
-                <h1>esquerda</h1>
+                <ContainerInfo titulo="Recommended Pages">
+                    <ContainerItem titulo="Fast Pizza" descricao="Pizza e Fast Food" icone={<PlusOutlined/>}/>
+                    <ContainerItem titulo="Lonely Droid" descricao="Technology" icone={<PlusOutlined/>}/>
+                    <ContainerItem titulo="Meta Movies" descricao="Movies / Entertainment" icone={<PlusOutlined/>}/>
+                </ContainerInfo>
+
+                <ContainerInfo titulo="Stories">
+                    <ContainerItem titulo="Gabriel" descricao="1h" />
+                    <ContainerItem titulo="Lucas" descricao="3h" />
+                    <ContainerItem titulo="Joana" descricao="1d" />
+                </ContainerInfo>
+                
             </div>
+
             <div className="container-center">
-                <h1>feed</h1>
+                <div className="componente-a"></div>
+                <div className="componente-a"></div>
+                <div className="componente-a"></div>
+                <div className="componente-a"></div>
             </div>
+
             <div className="container-right">
-                <h1>direita</h1>
+                <ContainerInfo titulo="Stories">
+                    <ContainerItem titulo="Gabriel" descricao="1h" />
+                    <ContainerItem titulo="Lucas" descricao="3h" />
+                    <ContainerItem titulo="Joana" descricao="1d" />
+                </ContainerInfo>
             </div>
         </div>
     )
