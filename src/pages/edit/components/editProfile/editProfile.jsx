16fac16973698a10f6605646bee554e8de785558
FShinoda <fernanda.shinoda@hotmail.com>
2021-05-07 16:11:15 -0300

    edit page details added

--- src/pages/edit/components/editProfile/editProfile.jsx
+++ src/pages/edit/components/editProfile/editProfile.jsx
@@ -4,15 +4,15 @@ import './style.css';
 // assets
 import UserImg from '../../../../assets/userPlaceholder.jpg';
 
-const EditProfile = ({fullname, username, bio, city, state}) =>{
+const EditProfile = ({fullname, username, bio, occupation, city, state}) =>{
 
     const [newFullname, setNewFullname] = useState(fullname);
     const [newUsername, setNewUsername] = useState(username);
     const [newBio, setNewBio] = useState(bio);
+    const [newOccupation, setNewOccupation] = useState(occupation);
     const [newCity, setNewCity] = useState(city);
     const [newState, setNewState] = useState(state);
     
-
     return(
         <div className="EditProfile">
             <div className="editProfile-show">
@@ -22,7 +22,7 @@ const EditProfile = ({fullname, username, bio, city, state}) =>{
                     <p className="username">@{newUsername}</p>
                 </div>
                 <div className="editUser-card-info">
-                    <p className="user-type">Estudante</p>
+                    <p className="user-type">{newOccupation}</p>
                     <p className="bio">{newBio}</p>
                     <p className="local">{newCity ? (newState ? (`${newCity}, ${newState}`) : newCity) : newState}</p>
                 </div>
@@ -30,30 +30,37 @@ const EditProfile = ({fullname, username, bio, city, state}) =>{
             <form className="editUser-form">
                 <div className="editUser-edit">
                     <label for="fullname">Nome</label>
-                    <input type="text" id="fullname" placeholder="nome completo" value={newFullname} onChange={e => setNewFullname(e.target.value)}/>
+                    <input type="text" id="fullname" placeholder="nome completo" maxLength="45" value={newFullname} onChange={e => setNewFullname(e.target.value)}/>
                 </div>
                 <div className="editUser-edit">
                     <label for="username">Nome de usuário</label>
-                    <input type="text" id="username" placeholder="usuário" value={newUsername}  onChange={e => setNewUsername(e.target.value)} />
+                    <input type="text" id="username" placeholder="usuário" maxLength="45" value={newUsername}  onChange={e => setNewUsername(e.target.value)} />
                 </div>
 
                 <div className="editUser-edit">
                     <label for="bio">Bio</label>
-                    <textarea id="bio" value={newBio} onChange={e => setNewBio(e.target.value)} />
+                    <textarea id="bio" value={newBio} maxLength="180" onChange={e => setNewBio(e.target.value)} />
+                </div>
+
+                <div className="editUser-edit">
+                    <label for="occupation">Ocupação</label>
+                    <input type="text" id="occupation" maxLength="45" placeholder="ex: Estudante" value={newOccupation} onChange={e => setNewOccupation(e.target.value)}/>
                 </div>
 
                 <div className="editUser-edit">
                     <label for="city">Cidade</label>
-                    <input type="text" id="city" placeholder="ex: Santo André" value={newCity} onChange={e => setNewCity(e.target.value)}/>
+                    <input type="text" id="city" maxLength="45" placeholder="ex: Santo André" value={newCity} onChange={e => setNewCity(e.target.value)}/>
                 </div>
                 <div className="editUser-edit">
                     <label for="state">Estado</label>
-                    <input type="text" id="state" placeholder="ex: São Paulo" value={newState} onChange={e => setNewState(e.target.value)}/>
+                    <input type="text" id="state" maxLength="45" placeholder="ex: São Paulo" value={newState} onChange={e => setNewState(e.target.value)}/>
                 </div>
+
+                <h4>Informações privadas</h4>
 
                 <div className="editUser-edit">
                     <label for="email">E-mail</label>
-                    <input type="email" id="email" placeholder="e-mail para contato"/>
+                    <input type="email" id="email" maxLength="45" placeholder="e-mail para contato"/>
                 </div>
             </form>
             
