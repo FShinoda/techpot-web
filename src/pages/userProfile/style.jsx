dbbb091c632ac5b3c12c3f11a8b02964f6d662b4
João Kitajima <joao.kitajima@outlook.com>
2021-05-09 18:44:39 -0300

    commit teste

--- src/pages/userProfile/style.jsx
+++ src/pages/userProfile/style.jsx
@@ -6,9 +6,10 @@ const UserProfile = () => {
         <div id="UserProfile">
 
             <div className="textBox">
-                <h2>UM TÍTULO DE PARÁGRAFO AQUI</h2>
+                <h2>TESTE AQUI</h2>
+                <h2>TESTE AQUI</h2>
+                <h2>TESTE AQUI</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sagittis vehicula purus eleifend varius. Aliquam placerat risus non tortor mollis, eu luctus ipsum tincidunt. Maecenas massa dui, semper sit amet vulputate at, tincidunt in purus. Praesent ultrices est vitae felis ornare, sed dictum sem facilisis. Fusce id lorem auctor, molestie ante vitae, mattis libero. Fusce at consequat libero, et volutpat erat. Quisque sit amet aliquet est. Vivamus quis neque risus. Integer maximus venenatis erat, ac lacinia lorem egestas in. Morbi sed tincidunt ante. Cras sodales, lorem a egestas consequat, dolor dolor accumsan dui, in blandit augue tellus sed tortor. Nulla condimentum vel augue et laoreet. Etiam sapien tellus, venenatis vel eleifend vel, sagittis sed felis. Maecenas ac velit lobortis, placerat quam sit amet, porta lacus.</p>
             </div>
 
             <div className="textBox">
