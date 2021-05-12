85b3a335c63d14485ccb0b2805d72f1106bbe383
FShinoda <fernanda.shinoda@hotmail.com>
2021-05-12 19:57:53 -0300

    groups desk init

--- src/pages/groupSearchMob/groupSearch.jsx
+++ src/pages/groupSearchMob/groupSearch.jsx
@@ -12,6 +12,7 @@ import {LeftOutlined} from '@ant-design/icons';
 
 
 const GroupSearch = () =>{
+
     return(
         <div className="GroupSearch">
             <div className="GroupSearch-header">
