647193a5538e5eba45834f682de7486e725ae617
João Kitajima <joao.kitajima@outlook.com>
2021-05-12 02:05:46 -0300

    userProfile alter

--- src/pages/userProfile/userProfile.jsx
+++ src/pages/userProfile/userProfile.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+/* eslint-disable eqeqeq */
+import React, { useState } from "react";
 import "./userProfile.css";
+
+
+/* COMPONENTS */
+import UserInfoNavItemList from "./components/userInfoNavItemList/userInfoNavItemList.jsx";
 
 
 /* IMAGES */
@@ -7,63 +12,42 @@ import profileBackgroundImage from "../../assets/profileBackgroundImage.jpg";
 import userProfilePlaceholder from "../../assets/userProfilePlaceholder.jpg";
 
 
-/* ICONS */
-//
-
-
 /* USER PROFILE PAGE */
 const UserProfile = () => {
-    // Owner or Guest Verification
-    let isOwner = false;
+    // User Info Navigation
+    const [ currentNav, setCurrentNav ] = useState("1");
 
+    const handleClick = (event) => {
+        console.log(event.target.key);
+        setCurrentNav(event.target.key);
+    };
 
-    // Friend Verification
-    let isFriend = false;
+    const getNavContent = (current) => {
+        switch(current) {
+            case "1":
+                return <div style={{
+                    height: "25px", width: "50px", backgroundColor: "#d0094d"
+                }}></div>;
+            case "2":
+                return <div style={{
+                    height: "25px", width: "50px", backgroundColor: "#361d04"
+                }}></div>;
+            default:
+                break;
+        }
+    };
 
 
     return(
         <div className="UserProfile">
-            
-            
             {/* BACKGROUND IMAGE AND PROFILE PHOTO */}
             <div className="graphical">
                 <div className="background">
                     <img className="backgroundImg" src={profileBackgroundImage} alt="Imagem de fundo do perfil do usuário." />
-                    {
-                        isOwner
-                        ?
-                            <div className="backgroundImgCameraIcon">
-                                {/* <UpdateImageModal /> */}
-                            </div> 
-                        :
-                            null
-                    }
                 </div>
 
                 <div className="profileImg">
                     <img className="userImg" src={userProfilePlaceholder} alt="Imagem de perfil do usuário." />
-                    {
-                        isOwner
-                        ?
-                            <div className="profilePhoto flex-center">
-                                {/* <UpdateImageModal /> */}
-                            </div>
-                        :
-                            null
-                    }
-
-                    {
-                        isFriend
-                        ?
-                            null
-                        :
-                            <div className="iconAdd flex-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="iconAddSymbol">
-                                    <line x1="12" y1="5" x2="12" y2="19"></line>
-                                    <line x1="5" y1="12" x2="19" y2="12"></line>
-                                </svg>
-                            </div>
-                    }
                 </div>
             </div>
 
@@ -71,7 +55,21 @@ const UserProfile = () => {
             {/* NAME AND IDENDITY */}
             <div className="profileId">
                 <h2>Nome Sobrenome</h2>
+                <h3>@username</h3>
                 <span>Sistemas de Informação</span>
+            </div>
+            
+
+            {/* USER INFO NAVIGATION */}
+            <div className="userInfoNav">
+                <div className="listNav">
+                    <UserInfoNavItemList key="1" title={"Visão Geral"} clickEvent={() => setCurrentNav("1")} isSelected={currentNav == "1" ? true : false} />
+                    <UserInfoNavItemList key="2" title={"Sobre"} clickEvent={() => setCurrentNav("2")} isSelected={currentNav == "2" ? true : false} />
+                </div>
+
+                <div className="content">
+                    {getNavContent(currentNav)}
+                </div>
             </div>
         </div>
     );
