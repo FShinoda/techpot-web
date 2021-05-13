cb62dbc650fe2229acc5dd6532125c337ea13842
FShinoda <fernanda.shinoda@hotmail.com>
2021-05-13 20:09:36 -0300

    Edit page conditional rendering done

--- src/pages/generalEdit/generalEdit.jsx
+++ src/pages/generalEdit/generalEdit.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css';
 
 
@@ -9,6 +9,12 @@ import EditItem from './components/editItem/editItem.jsx'
 import { LeftOutlined, UserOutlined, BellOutlined, AuditOutlined, LockOutlined } from '@ant-design/icons';
 
 const GeneralEdit = () =>{
+
+    const [isAccountOpen, setIsAccountOpen] = useState(false);
+    const [isProfileOpen, setIsProfileOpen] = useState(false);
+    const [isPortfolioOpen, setIsPortfolioOpen] = useState(false);
+    const [isNotifOpen, setIsNotifOpen] = useState(false);
+
     return(
         <div className="GeneralEdit">
             <div className="GeneralEdit-header">
@@ -17,10 +23,14 @@ const GeneralEdit = () =>{
             </div>
 
             <div className="GeneralEdit-container">
-                <EditItem icon={<LockOutlined />} title={"Minha conta"}/> 
-                <EditItem icon={<UserOutlined/>} title={"Meu perfil"}/> 
-                <EditItem icon={<AuditOutlined />} title={"Meu Portifólio"}/> 
-                <EditItem icon={<BellOutlined />} title={"Notificações"}/> 
+                <EditItem icon={<LockOutlined />} title={"Minha conta"} setIsActive={() => {setIsAccountOpen(!isAccountOpen)}} isActive={isAccountOpen}/>
+                {isAccountOpen ? ('a') : null} 
+                <EditItem icon={<UserOutlined/>} title={"Meu perfil"} setIsActive={() => {setIsProfileOpen(!isProfileOpen)}} isActive={isProfileOpen}/>
+                {isProfileOpen ? ('b') : null}  
+                <EditItem icon={<AuditOutlined />} title={"Meu Portifólio"} setIsActive={() => {setIsPortfolioOpen(!isPortfolioOpen)}} isActive={isPortfolioOpen}/>
+                {isPortfolioOpen ? ('c') : null}   
+                <EditItem icon={<BellOutlined />} title={"Notificações"} setIsActive={() => {setIsNotifOpen(!isNotifOpen)}} isActive={isNotifOpen}/> 
+                {isNotifOpen ? ('d') : null}  
                 
                 <button className="GeneralEdit-btn-logout">Log out</button>
             
