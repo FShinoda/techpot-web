7184545897031e718eaa0914083bfb9bcf7612f3
João Kitajima <joao.kitajima@outlook.com>
2021-05-13 18:04:15 -0300

    userProfile update

--- src/pages/userProfile/userProfile.jsx
+++ src/pages/userProfile/userProfile.jsx
@@ -21,16 +21,16 @@ import icon from "../../assets/icone_lupa.png";
 const UserProfile = () => {
     // User Info Navigation
     const [ currentNav, setCurrentNav ] = useState("1");
+    
 
-    const handleClick = (event) => {
-        console.log(event.target.key);
-        setCurrentNav(event.target.key);
-    };
+    // Owner and Guest Verification
+    let isOwner = true;
+
 
     const getNavContent = (current) => {
         switch(current) {
             case "1":
-                return <UserOverview/>;
+                return <UserOverview />;
             case "2":
                 return <div style={{
                     height: "25px", width: "50px", backgroundColor: "#cccccc"
@@ -64,17 +64,16 @@ const UserProfile = () => {
             <div className="profileId">
                 <h2>João Kitajima</h2>
                 <h3>@kitajima</h3>
-                <span>Sistemas de Informação</span>
             </div>
             
 
             {/* USER INFO NAVIGATION */}
             <div className="userInfoNav">
                 <div className="listNav">
-                    <UserInfoNavItemList key="1" icon={icon} title={"Visão Geral"} alt={"Ícone X."} clickEvent={() => setCurrentNav("1")} isSelected={currentNav == "1" ? true : false} />
-                    <UserInfoNavItemList key="2" icon={icon} title={"Grupos"} alt={"Ícone X."} clickEvent={() => setCurrentNav("2")} isSelected={currentNav == "2" ? true : false} />
-                    <UserInfoNavItemList key="3" icon={icon} title={"Amigos"} alt={"Ícone X."} clickEvent={() => setCurrentNav("3")} isSelected={currentNav == "3" ? true : false} />
-                    <UserInfoNavItemList key="4" icon={icon} title={"Portfolio"} alt={"Ícone X."} clickEvent={() => setCurrentNav("4")} isSelected={currentNav == "4" ? true : false} />
+                    <UserInfoNavItemList icon={icon} title={"Visão Geral"} alt={"Ícone X."} clickEvent={() => setCurrentNav("1")} isSelected={currentNav == "1" ? true : false} />
+                    <UserInfoNavItemList icon={icon} title={"Grupos"} alt={"Ícone X."} clickEvent={() => setCurrentNav("2")} isSelected={currentNav == "2" ? true : false} />
+                    <UserInfoNavItemList icon={icon} title={"Amigos"} alt={"Ícone X."} clickEvent={() => setCurrentNav("3")} isSelected={currentNav == "3" ? true : false} />
+                    <UserInfoNavItemList icon={icon} title={"Portfolio"} alt={"Ícone X."} clickEvent={() => setCurrentNav("4")} isSelected={currentNav == "4" ? true : false} />
                 </div>
 
                 <div className="content">
