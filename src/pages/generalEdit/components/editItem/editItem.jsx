1eff2eae161739d59cdc55e504315e4c6b2a5132
FShinoda <fernanda.shinoda@hotmail.com>
2021-05-13 20:09:36 -0300

    Edit page conditional rendering done

--- src/pages/generalEdit/components/editItem/editItem.jsx
+++ src/pages/generalEdit/components/editItem/editItem.jsx
@@ -4,16 +4,16 @@ import './style.css';
 // Icon
 import { RightOutlined } from '@ant-design/icons';
 
-const EditItem = ({icon, title}) =>{
+const EditItem = ({icon, title, setIsActive, isActive}) =>{
     return(
-        <div className="EditItem">
+        <div className="EditItem" onClick={setIsActive}>
                 
             <div className="EditItem-title">
                 <span>{icon}</span>
                 <span className="EditItem-title-padding">{title}</span>
             </div>
             <div className="EditItem-drop">
-                <RightOutlined />
+                <RightOutlined className={ isActive ? ("EditItem-active") : null}/>
             </div>
         </div>
     )
