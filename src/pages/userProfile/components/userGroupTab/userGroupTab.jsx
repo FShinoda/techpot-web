d0a4a5cab89e0cdf6b2956fabacfb617554d89f9
João Kitajima <joao.kitajima@outlook.com>
2021-05-14 00:42:31 -0300

    userGroupTab added

---
+++ src/pages/userProfile/components/userGroupTab/userGroupTab.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./userGroupTab.css";
+
+
+/* USER GROUP TAB */
+const UserGroupTab = () => {
+    return(
+        <div className="UserGroupTab"></div>
+    );
+};
+
+export default UserGroupTab;
