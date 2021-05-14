d722d6f053fd04df1c96030b9af8bf2c2749f873
FShinoda <fernanda.shinoda@hotmail.com>
2021-05-14 10:25:33 -0300

    profile edit form added

--- src/pages/generalEdit/profileEdit/profileEdit.jsx
+++ src/pages/generalEdit/profileEdit/profileEdit.jsx
@@ -12,25 +12,44 @@ import EditHeader from '../components/editHeader/editHeader.jsx';
 import UpdateImageModal from "../components/updateImgModal/updateImgModal.jsx";
 
 
-const ProfileEdit = () =>{
+const ProfileEdit = ({}) =>{
     return(
         <div className="ProfileEdit">
             <EditHeader title="Meu perfil"/>
 
             <div className="ProfileEdit-container">
+                {/* Preview - banner + foto do usuario + name + username*/}
                 <div className="ProfileEdit-preview">
                     <div className="ProfileEdit-graphical">
                         <div className="ProfileEdit-background">
+                            {/* Background img */}
                             <img className="ProfileEdit-backgroundImg" src={profileBackgroundImage} alt="Imagem de fundo do perfil do usuário." />
                             <UpdateImageModal className="ProfileEdit-bg-update"/>
                         </div>
-
                         <div className="ProfileEdit-profileImg">
+                            {/* Profile img */}
                             <img className="ProfileEdit-userImg" src={userPlaceholder} alt="Imagem de perfil do usuário." />
                         </div>
                     </div>
+                    <div className="ProfileEdit-profileId">
+                        {/* Fullname */}
+                        <h2>João Kitajima</h2>
+                        {/* Username */}
+                        <h3>@kitajima</h3>
+                    </div>
                 </div>
+
+                {/* Formulário de edição do perfil */}
+                <div className="ProfileEdit-edit">
+                    <form>
+                        <label for="">Nome</label>
+                        <input type="text"/>
+                    </form>
+                </div>
+
+
                 
+
             </div>
         </div>
     )
