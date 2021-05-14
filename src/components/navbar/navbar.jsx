4a8d285f24857ebffe528703d74abb635eb7fe79
antoniobarolo <antoniobarolog@gmail.com>
2021-05-14 16:01:27 -0300

    last aaa

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import './navbar.css';
 
+import Logo from '../../assets/logo-redborder.png';
+import Notification from "../../assets/icone_sino.png"
+import Message from "../../assets/icone_mensagem.png";
+import ProfPic from "../../assets/userProfilePlaceholder.jpg";
+import Search from "../../assets/icone_lupa.png";
+
 const Navbar = () => {
 
     return (
         <div className="Navbar">
             <div className="header-caixa">
-                <ul className="header-logo">
-                    <li><img src="../../assets/logo-redborder.png" alt="Logo Techpot" /></li>
-                </ul>
-
-                <div className="header-search">
-                    <input placeholder="Pesquisar..."></input>
+                <div className="header-user">
+                    <img src={ProfPic} alt="Icon-ProfilePic" />
                 </div>
-                <nav>
-                    <ul className="header-nav">
-                        <li><img src="../../assets/icone_sino.png" alt="Icon-Notification" width="30px" /></li>
-                        <li><img src="../../assets/icone_mensagem.png" alt="Icon-Message" /></li>
-                        <li>Olá, Surubatur</li>
-                        <li className="nav__item"><img src="../../assets/userProfilePlaceholder.png" alt="Icon-ProfilePic" /></li>
-                    </ul>
-                </nav>
+
+                <div className="header-lefticons"></div>
+
+                <div className="header-logo">
+                    <img id="logo" src={Logo} alt="Logo Techpot" />
+                </div>
+
+                <div className = "header-righticons">
+                        <input placeholder="Pesquisar..."></input>
+
+                        <img src={Notification} alt="Icon-Notification" width="30px" />
+                        <img src={Message} alt="Icon-Message" />
+                </div>
             </div>
         </div>
     )
