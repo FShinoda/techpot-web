398a5e2590809a75edd2e118cebff39df6fdef5d
FShinoda <fernanda.shinoda@hotmail.com>
2021-05-14 10:25:33 -0300

    profile edit form added

--- src/pages/generalEdit/profileEdit/profileEdit.jsx
+++ src/pages/generalEdit/profileEdit/profileEdit.jsx
@@ -31,13 +31,18 @@ import UpdateImageModal from "../components/updateImgModal/updateImgModal.jsx";
 >>>>>>> 67ea4ce... Profile edit init
 
 
+<<<<<<< HEAD
 const ProfileEdit = () =>{
 >>>>>>> 25a674d... edit mobile pages init
+=======
+const ProfileEdit = ({}) =>{
+>>>>>>> d722d6f... profile edit form added
     return(
         <div className="ProfileEdit">
             <EditHeader title="Meu perfil"/>
 
             <div className="ProfileEdit-container">
+<<<<<<< HEAD
 <<<<<<< HEAD
                 {/* Preview - banner + foto do usuario + name + username*/}
                 <div className="ProfileEdit-preview">
@@ -72,20 +77,44 @@ const ProfileEdit = () =>{
                 
 
 =======
+=======
+                {/* Preview - banner + foto do usuario + name + username*/}
+>>>>>>> d722d6f... profile edit form added
                 <div className="ProfileEdit-preview">
                     <div className="ProfileEdit-graphical">
                         <div className="ProfileEdit-background">
+                            {/* Background img */}
                             <img className="ProfileEdit-backgroundImg" src={profileBackgroundImage} alt="Imagem de fundo do perfil do usuário." />
                             <UpdateImageModal className="ProfileEdit-bg-update"/>
                         </div>
-
                         <div className="ProfileEdit-profileImg">
+                            {/* Profile img */}
                             <img className="ProfileEdit-userImg" src={userPlaceholder} alt="Imagem de perfil do usuário." />
                         </div>
                     </div>
+                    <div className="ProfileEdit-profileId">
+                        {/* Fullname */}
+                        <h2>João Kitajima</h2>
+                        {/* Username */}
+                        <h3>@kitajima</h3>
+                    </div>
                 </div>
+
+                {/* Formulário de edição do perfil */}
+                <div className="ProfileEdit-edit">
+                    <form>
+                        <label for="">Nome</label>
+                        <input type="text"/>
+                    </form>
+                </div>
+
+
                 
+<<<<<<< HEAD
 >>>>>>> 25a674d... edit mobile pages init
+=======
+
+>>>>>>> d722d6f... profile edit form added
             </div>
         </div>
     )
