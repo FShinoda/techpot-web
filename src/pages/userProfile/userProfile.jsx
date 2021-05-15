5315c8dcb462b2d763bc722302173fb697762116
João Kitajima <joao.kitajima@outlook.com>
2021-05-15 02:42:34 -0300

    userProfile update - v0.9.0

--- src/pages/userProfile/userProfile.jsx
+++ src/pages/userProfile/userProfile.jsx
@@ -7,6 +7,8 @@ import "./userProfile.css";
 import UserInfoNavItemList from "./components/userInfoNavItemList/userInfoNavItemList.jsx";
 import UserOverview from "./components/userOverview/userOverview.jsx";
 import UserGroupTab from "./components/userGroupTab/userGroupTab.jsx";
+import UserFriendTab from "./components/userFriendTab/userFriendTab.jsx";
+import UserPortfolio from "./components/userPortfolio/userPortfolio.jsx";
 
 
 /* IMAGES */
@@ -15,7 +17,7 @@ import userProfilePlaceholder from "../../assets/userProfilePlaceholder.jpg";
 
 
 /* ICONS */
-import icon from "../../assets/icone_lupa.png";
+import icon from "../../assets/icone_lupa-branco.png";
 
 
 /* USER PROFILE PAGE */
@@ -23,11 +25,6 @@ const UserProfile = () => {
     // User Info Navigation
     const [ currentNav, setCurrentNav ] = useState("1");
     
-
-    // Owner and Guest Verification
-    const [ friendStatus, setFriendStatus ] = useState("notFriend");
-
-
     const getNavContent = (current) => {
         switch(current) {
             case "1":
@@ -35,39 +32,43 @@ const UserProfile = () => {
             case "2":
                 return <UserGroupTab />;
             case "3":
-                return null;
+                return <UserFriendTab />;
             case "4":
-                return null;
+                return <UserPortfolio name={"JOÃO KITAJIMA"} spec={"Analista de Piadas Ruins"} />;
             default:
                 break;
         }
     };
 
 
+    // Friend Status
+    const [ friendStatus, setFriendStatus ] = useState("notFriend");
+    
+
     return(
         <div className="UserProfile">
             {/* BACKGROUND IMAGE AND PROFILE PHOTO */}
-            <div className="graphical">
-                <div className="background">
-                    <img className="backgroundImg" src={profileBackgroundImage} alt="Imagem de fundo do perfil do usuário." />
-                    <div className="middleSection">
-                        <code className="friendCount">0</code>
+            <div className="UserProfile_graphical">
+                <div className="UserProfile_background">
+                    <img className="UserProfile_backgroundImg" src={profileBackgroundImage} alt="Imagem de fundo do perfil do usuário." />
+                    <div className="UserProfile_middleSection">
+                        <code className="UserProfile_friendCount">0</code>
                         {
                             friendStatus == "notFriend"
                             ?
-                                <button className="defaultFriend" onClick={() => setFriendStatus("sendingFriend")}>
+                                <button className="UserProfile_defaultFriend" onClick={() => setFriendStatus("sendingFriend")}>
                                     <img src={icon} alt="Adicionar nova amizade." />
                                     <span>Adicionar</span>
                                 </button>
                             :
                                 friendStatus == "sendingFriend"
                                 ?
-                                    <button className="defaultFriend sendingFriend" onClick={() => setFriendStatus("isFriend")}>
+                                    <button className="UserProfile_defaultFriend UserProfile_sendingFriend" onClick={() => setFriendStatus("isFriend")}>
                                         <img src={icon} alt="Cancelar solicitação." />
                                         <span>Cancelar solicitação</span>
                                     </button>
                                 :
-                                    <button className="defaultFriend isFriend" onClick={() => setFriendStatus("notFriend")}>
+                                    <button className="UserProfile_defaultFriend UserProfile_isFriend" onClick={() => setFriendStatus("notFriend")}>
                                         <img src={icon} alt="Desfazer amizade." />
                                         <span>Desfazer amizade</span>
                                     </button>
@@ -75,29 +76,29 @@ const UserProfile = () => {
                     </div>
                 </div>
 
-                <div className="profileImg">
-                    <img className="userImg" src={userProfilePlaceholder} alt="Imagem de perfil do usuário." />
+                <div className="UserProfile_profileImg">
+                    <img className="UserProfile_userImg" src={userProfilePlaceholder} alt="Imagem de perfil do usuário." />
                 </div>
             </div>
 
 
             {/* NAME AND IDENDITY */}
-            <div className="profileId">
+            <div className="UserProfile_profileId">
                 <h2>João Kitajima</h2>
-                <h3>@kitajima</h3>
+                <h3>kitajima</h3>
             </div>
             
 
             {/* USER INFO NAVIGATION */}
-            <div className="userInfoNav">
-                <div className="listNav">
-                    <UserInfoNavItemList icon={icon} title={"Visão Geral"} alt={"Ícone X."} clickEvent={() => setCurrentNav("1")} isSelected={currentNav == "1" ? true : false} />
-                    <UserInfoNavItemList icon={icon} title={"Grupos"} alt={"Ícone X."} clickEvent={() => setCurrentNav("2")} isSelected={currentNav == "2" ? true : false} />
-                    <UserInfoNavItemList icon={icon} title={"Amigos"} alt={"Ícone X."} clickEvent={() => setCurrentNav("3")} isSelected={currentNav == "3" ? true : false} />
-                    <UserInfoNavItemList icon={icon} title={"Portfolio"} alt={"Ícone X."} clickEvent={() => setCurrentNav("4")} isSelected={currentNav == "4" ? true : false} />
+            <div className="UserProfile_userInfoNav">
+                <div className="UserProfile_listNav">
+                    <UserInfoNavItemList icon={icon} title={"Visão Geral"} alt={"Ícone de Visão Geral."} clickEvent={() => setCurrentNav("1")} isSelected={currentNav == "1" ? true : false} />
+                    <UserInfoNavItemList icon={icon} title={"Grupos"} alt={"Ícone de Grupos."} clickEvent={() => setCurrentNav("2")} isSelected={currentNav == "2" ? true : false} />
+                    <UserInfoNavItemList icon={icon} title={"Amigos"} alt={"Ícone de Amigos."} clickEvent={() => setCurrentNav("3")} isSelected={currentNav == "3" ? true : false} />
+                    <UserInfoNavItemList icon={icon} title={"Portfolio"} alt={"Ícone de Portfolio."} clickEvent={() => setCurrentNav("4")} isSelected={currentNav == "4" ? true : false} />
                 </div>
 
-                <div className="content">
+                <div className="UserProfile_content">
                     {getNavContent(currentNav)}
                 </div>
             </div>
