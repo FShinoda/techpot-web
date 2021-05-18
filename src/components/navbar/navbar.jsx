69ee85d8b8c4a712775493b702375a2f60443f57
antoniobarolo <antoniobarolog@gmail.com>
2021-05-18 11:32:55 -0300

    o

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,6 +5,8 @@ import Logo from '../../assets/logo-redborder.png';
 import Notification from "../../assets/icone_sino.png"
 import Message from "../../assets/icone_mensagem.png";
 import ProfPic from "../../assets/userProfilePlaceholder.jpg";
+import GroupPic from "../../assets/icone_grupo.png";
+import Calendar from "../../assets/icone_evento.png";
 import Search from "../../assets/icone_lupa.png";
 
 const Navbar = () => {
@@ -12,21 +14,24 @@ const Navbar = () => {
     return (
         <div className="Navbar">
             <div className="header-caixa">
-                <div className="header-user">
-                    <img src={ProfPic} alt="Icon-ProfilePic" />
+                <div className="header-lefticons">
+                    <div className="header-user">
+                        <img src={ProfPic} alt="Icon-ProfilePic" />
+                        <p>Anônimo</p>
+                    </div>
+                    <img src={GroupPic} alt="Icon-GroupPic" />
+                    <img src={Calendar} alt="Icon-Calendar" />
                 </div>
-
-                <div className="header-lefticons"></div>
 
                 <div className="header-logo">
                     <img id="logo" src={Logo} alt="Logo Techpot" />
                 </div>
 
-                <div className = "header-righticons">
-                        <input placeholder="Pesquisar..."></input>
+                <div className="header-righticons">
+                    <input placeholder="Pesquisar..."></input>
 
-                        <img src={Notification} alt="Icon-Notification" width="30px" />
-                        <img src={Message} alt="Icon-Message" />
+                    <img src={Notification} alt="Icon-Notification"/>
+                    <img src={Message} alt="Icon-Message" />
                 </div>
             </div>
         </div>
